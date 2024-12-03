e86217b1765981c756ed71e2f93c6e984586f6b6
Matei Cloteaux <matei.cloteaux@gmail.com>
2024-12-03 00:45:25 -0500

    💻 day3

---
+++ q03.rkt
@@ -0,0 +1,35 @@
+#lang racket
+
+(require racket/set)
+(require "utils.rkt")
+
+(in-file "in.txt")
+
+;; Part One                
+(define mul-re #rx"mul\\([0-9]+,[0-9]+\\)")
+
+(define (sum-line line)
+  (sum (for/list ([instr (regexp-match* mul-re line)])
+         (foldr * 1 (map string->number
+                         (regexp-match* #rx"[0-9]+" instr))))))
+
+(define input (foldr string-append "" lines))
+
+(printf "Part one: ~a\n" (sum-line input)#;(sum (map sum-line lines)))
+
+;; Part Two
+
+(define (dos in do?)
+  (cond
+    ((null? in) '())
+    (else
+     (define splt (string-split in (if do? "don't()" "do()")))
+     (cond
+       ((null? splt) '())
+       (else
+        (define ret (dos (foldr (λ (a b)
+                                  (string-append a (if do? "don't()" "do()") b))
+                                "" (cdr splt)) (not do?)))
+        (if do? (cons (car splt) ret) ret))))))
+
+(printf "Part two: ~a\n" (sum (map sum-line (dos input #t))))
